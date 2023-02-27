b3e005b74a9c0bf0d429049ad8839619bc0da5b8
Deepak <dbehera647@gmail.com>
2023-02-27 19:53:14 +0530

    complted building the banner section:

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const apiEndpoint = "https://api.themoviedb.org/3";
 const imgPath = "https://image.tmdb.org/t/p/original";
 const apiPaths = {
   fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`,
-  // fetchTrending :
+  fetchTrending :`${apiEndpoint}/trending/all/day?api_key=${apikey}&language=en-US`,
   // fetchLatest:
   // fetchNetflixOriginals:
   // fetchTopRated:
@@ -15,19 +15,48 @@ const apiPaths = {
 
 // // boots up the app
 function init() {
-  fetchAndBuildAllSections();
+    fetchTrendingMovies();
+    fetchAndBuildAllSections();
 }
+function fetchTrendingMovies(){
+    fetchAndbuildMovieSection(apiPaths.fetchTrending,'Trending Now')
+    .then(list => {
+        const randomIndex =parseInt(Math.random()*list.length);
+        buildBannerSection(list[randomIndex])
+    }).catch(err =>{
+        console.error(err)
+    })
 
+}
+function buildBannerSection(movie){
+    const bannerCont =document.querySelector('.banner-section');
+    bannerCont.style.backgroundImage = `url('${imgPath}${movie.backdrop_path}')`;
+    const div =document.createElement('div');
+    div.innerHTML =`
+         <h2 class="banner-title">${movie.title}</h2>
+        <p class="banner-info" >Rating ${movie.vote_average}</p>
+        <p class="banner-overview" >${movie.overview}</p>
+        <div class="action-buttons">
+            <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg>&nbsp;&nbsp;Play</button>
+            <button class="action-button" ><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg>&nbsp;&nbsp;More Info</button>
+        </div>
+        
+    
+    `;
+    div.className="banner-content container"
+    bannerCont.append(div);
+
+}
 function fetchAndBuildAllSections() {
   fetch(apiPaths.fetchAllCategories)
     .then((res) => res.json())
     .then((res) => {
       const categories = res.genres;
       if (Array.isArray(categories) && categories.length) {
-        categories.forEach((category) => {
+        categories.slice(0,3).forEach((category) => {
           fetchAndbuildMovieSection(
             apiPaths.fetchMoviesList(category.id),
-            category
+            category.name
           );
         });
       }
@@ -35,18 +64,20 @@ function fetchAndBuildAllSections() {
     .catch((err) => console.error(err));
 }
 
-function fetchAndbuildMovieSection(fetchUrl, category) {
-  console.log(fetchUrl, category);
-  fetch(fetchUrl)
+function fetchAndbuildMovieSection(fetchUrl, categoryName) {
+  console.log(fetchUrl, categoryName);
+  return fetch(fetchUrl)
     .then((res) => res.json())
     .then((res) => {
       // console.table(res.results);
       const movies = res.results;
       if (Array.isArray(movies) && movies.length) {
-        buildMoviesSection(movies, category.name);
+        buildMoviesSection(movies, categoryName);
       }
+      return movies;
     })
-    .catch((err) => console.error(err));
+    .catch((err) => console.error(err))
+ 
 }
 
 function buildMoviesSection(list, categoryName) {
@@ -67,7 +98,7 @@ function buildMoviesSection(list, categoryName) {
         </div>
      
     `;
-  console.log(moviesSectionHTML);
+ 
 
   const div = document.createElement("div");
   div.className = "movies-section";
